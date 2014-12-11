c4b3101cb2cdc71aa2fd23991ee2b2fbe092863b
Michael Goldstein <michael@bitstein.org>
2014-12-10 22:04:49 -0600

    Update script, fixes #1

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ This Chrome extension uses regex to replace any mention of "Bitcoin" with "Bitco
 
 1. Git clone or download the zip and extract into a folder.
 2. In `chrome://extensions/`, click "Load unpacked extension..." and find the folder from 1.
-3. Have a sigh of relief. (Sometimes you might need to refresh first.)
+3. Have a sigh of relief.
 
 Note: If you happen to want to see the actual words, you will need to disable the extension and reload the page.
 
-Known bug: may interfere with other JavaScript loaded on the page.
+Based on panicsteve's "cloud-to-butt": https://github.com/panicsteve/cloud-to-butt
--- myscript.js
+++ myscript.js
@@ -1,3 +1,41 @@
-var reg = "blockchain technology|block chain technology|blockchain tech|block chain tech"
+// Copied from these copiers: https://github.com/panicsteve/cloud-to-butt/blob/master/Source/content_script.js
+walk(document.body);
 
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp("("+ reg + ")", "gi"), "Bitcoin");
+function walk(node) 
+{
+  // I stole this function from here:
+  // http://is.gd/mwZp7E
+  
+  var child, next;
+
+  switch ( node.nodeType )  
+  {
+    case 1:  // Element
+    case 9:  // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while ( child ) 
+      {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
+}
+
+function handleText(textNode) 
+{
+  var v = textNode.nodeValue;
+
+  v = v.replace(/\bblockchain technology\b/i, "Bitcoin");
+  v = v.replace(/\bblock chain technology\b/i, "Bitcoin");
+  v = v.replace(/\bblockchain tech\b/i, "Bitcoin");
+  v = v.replace(/\bblock chain tech\b/i, "Bitcoin");
+  
+  textNode.nodeValue = v;
+}
